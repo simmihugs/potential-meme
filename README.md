9f783041ca3b351d04338490877b9d37c17fdce7
simmihugs <simon.graetz@gmx.de>
2025-06-18 20:46:36 +0200

    updated logging

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# potential-meme
-Fastapi within a docker container
+# Fastapi within a docker container
+
+The purpose of this repo is to explore a fastapi within a docker container, where the python files 
+can be eddited while the docker image is running and all changes are reloaded correctly.
+
--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
+import logging
 from fastapi import FastAPI
 from .routers import agent
 from .routers import second_agent
+
+LOG_FORMAT = "%(levelname)s:     %(message)s"
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+logger = logging.getLogger(__name__)
+
 
 app = FastAPI()
 app.include_router(agent.router)
